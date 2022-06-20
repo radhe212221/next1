2666ddf57e45b523cb94e654dea44499439ffe3d
RS <radhe212221@gmail.com>
2022-06-20 16:11:46 +0530

    added back button

--- src/views/user-view.js
+++ src/views/user-view.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import {  useRouter } from "next/router";
+import React from "react";
 
 export default function UserView(data) {
+  const router=useRouter()
   return (
-    <div>UserView : {JSON.stringify(data?.data)}</div>
-  )
+    <>
+      <button onClick={()=>router.push("/")}>back</button>
+      <div>UserView : {JSON.stringify(data?.data)}</div>
+    </>
+  );
 }
